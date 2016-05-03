b8274501382993bd78fa370b3cbf0999bfe15826
Dmitry Sky <dmitry.sky@icloud.com>
2016-05-02 19:03:22 -0600

    import jQuery and make Ajax call to skill-repo api for all of the skills

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import $ from 'jquery'
 import Note from './Note.jsx';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    $.getJSON('https://skill-repo.herokuapp.com/api/v1/skills.json', (response) => { console.log(response) });
+  }
+
   render() {
-    return <Note />;
+    // return <Note />;
+    return <div>Base React Template</div>;
   }
 }
