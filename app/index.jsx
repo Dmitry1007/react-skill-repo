b6689da159465fbc4ca1f8afe4b9480753e89f6f
Dmitry Sky <dmitry.sky@icloud.com>
2016-05-05 11:38:53 -0600

    import local js libraries

--- app/index.jsx
+++ app/index.jsx
@@ -5,6 +5,14 @@ import './css/magnific-popup.css';
 import './css/salvattore.css';
 // import './css/style.css';
 
+// import './js/modernizr-2.6.2.min.js'
+import './js/jquery.min.js'
+import './js/jquery.easing.1.3.js'
+import './js/bootstrap.min.js'
+import './js/jquery.waypoints.min.js'
+import './js/jquery.magnific-popup.min.js'
+import './js/salvattore.min.js'
+import './js/main.js'
 
 import React from 'react';
 import ReactDOM from 'react-dom';
